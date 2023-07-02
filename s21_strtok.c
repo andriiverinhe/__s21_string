30676222d941619df5150178b8645414b6b1002d
Nena Maxie <nenamaxi@su-k4.surgut.21-school.ru>
2023-07-02 12:01:48 +0500

    s21

--- s21_strtok.c
+++ s21_strtok.c
@@ -1,43 +1,37 @@
 #include "s21_string.h"
 
-char *s21_strtok(char *str, const char *delim){
-    static char *next;
 
-    if(str != NULL)
-        next = str;
-    else if(next == NULL || *next == '\0')
-        return NULL;
+char *s21_strtok(char *str, const char *delim) {
+  static char *next;
+  register int ch;
 
-    char *ptr_start = next;
-    char *ptr_end = NULL;
-    while (*next != '\0')
-    {
-        for(const char *ptr_del = delim; *ptr_del != '\0'; ptr_del++){
-            if(*next == *ptr_del) {
-                ptr_end = next;
-                break;
-            }
-        }
-        if(ptr_end != NULL){
-            *ptr_end = '\0';
-            next = ptr_end + 1;
-            return ptr_start;
-        }
-        next++;
+  if (str == 0) {
+    str = next;
+  }
+  do {
+    if ((ch = *str++) == '\0') {
+      return 0;
     }
-    return ptr_start;    
+  } while (strchr(delim, ch));
+  --str;
+  next = str + strcspn(str, delim);
+  if (*next != 0) {
+    *next++ = 0;
+  }
+  return str;
 }
 
+
 int main() {
-    char str[25] = "test1|test2/test3/test4";
-    char del[2] = "/";
+    char str[29] = "apple,banana,grape";
+    char del[5] = ",";
+
     printf("str: %s\n", str);
     char *p = s21_strtok(str, del);
 
     while (p != NULL)
     {
-        printf("p: %s\n",p);
-        printf("str: %s\n",p);
+        printf("%s\n",p);
         p =  s21_strtok(NULL, del);
     }
     
